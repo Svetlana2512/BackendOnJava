a2d3ea9584b83c91f1858e4fd91c26460bef980d
Svetlana2512 <dellenn@mail.ru>
2022-09-16 15:00:22 +0400

    Initial commit

---
+++ src/main/java/BackendOnJava/PalindromeChecker.java
@@ -0,0 +1,29 @@
+package BackendOnJava;
+
+import java.io.*;
+
+public class PalindromeChecker {
+
+    // Там холм лохмат - Tam xolm loxmaT
+
+    public static boolean isPalindrome(String str) {
+        String rev = "";
+        boolean ans = false;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+        if (str.equals(rev)) {
+            ans = true;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args)
+    {
+        String str = "TamxolmloxmaT";
+
+        str = str.toLowerCase();
+        boolean A = isPalindrome(str);
+        System.out.println(A);
+   }
+}
---
+++ src/test/java/BackendOnJavaTest/PalindromeCheckerTest.java
@@ -0,0 +1,11 @@
+package BackendOnJavaTest;
+
+import org.junit.jupiter.api.Test;
+
+public class PalindromeCheckerTest {
+
+    @Test
+    void baseTest() {
+
+    }
+}

115ee152ab85d018a089211d2f5813ddc7ad98e4
Svetlana2512 <dellenn@mail.ru>
2022-09-28 15:26:13 +0400

    DZ #6

--- src/main/java/Builder/Main.java
+++ src/main/java/Builder/Main.java
@@ -6,10 +6,15 @@ import Lesson4.ApiUserConnectResult;
 import Lesson4.SpoonaccularService;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 import java.io.IOException;
 
 
-public class Main {
+//public class Main {
 
 //    public static void main(String[] args) {
 //
@@ -25,14 +30,39 @@ public class Main {
 //                        .build();
 //    }
 
+//    public static void main(String[] args) throws IOException {
+//
+//        SpoonaccularService spoonaccularService = new SpoonaccularService();
+//        ApiSearchResult recipes = spoonaccularService.findRecipes("Bread", 3);
+//        System.out.println(recipes);
+//
+//        ApiUserConnectResult connectResult = spoonaccularService.connect(new ApiUserConnectRequest());
+//        System.out.println(connectResult);
+//
+//}
+//}
+public class Main {
     public static void main(String[] args) throws IOException {
 
-        SpoonaccularService spoonaccularService = new SpoonaccularService();
-        ApiSearchResult recipes = spoonaccularService.findRecipes("Bread", 3);
-        System.out.println(recipes);
+//        SpoonaccularService spoonaccularService = new SpoonaccularService();
+//        ApiSearchResult recipes = spoonaccularService.findRecipes("Bread", 3);
+//        System.out.println(recipes);
+//
+//        ApiUserConnectResult connectResult = spoonaccularService.connect(new ApiUserConnectRequest());
+//        System.out.println(connectResult);
 
-        ApiUserConnectResult connectResult = spoonaccularService.connect(new ApiUserConnectRequest());
-        System.out.println(connectResult);
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder()
+                .build(Resources.getResourceAsStream("myBatisConfig.xml"));
 
-}
+        try (SqlSession session = sessionFactory.openSession()) {
+            ProductsMapper productsMapper = session.getMapper(ProductsMapper.class);
+            CategoriesMapper categoriesMapper = session.getMapper(CategoriesMapper.class);
+            Products product = productsMapper.selectByPrimaryKey(444L);
+            System.out.println(product);
+            Categories category = categoriesMapper.selectByPrimaryKey(product.getCategoryId());
+            System.out.println(category);
+
+        }
+
+    }
 }
